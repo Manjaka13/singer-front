d26aaa7976632f5858ce652c0a3dbea7c7dd9fec
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-12-17 12:13:59 +0100

    add page concessionnaire

---
+++ pages/devenir-concessionnaire.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Page from "components/Page";
+import Devenirconcessionnaire from "components/Devenirconcessionnaire";
+
+/*
+	Home page
+*/
+
+const Home = (): JSX.Element => {
+	return (
+		<Page
+			title="Devenir concessionnaire Singer France"
+			description="Découvrez comment devenir concesssionnaire Singer, nous vous asssistons dans vos démarches."
+			image="/cover/devenir-concessionnaire.jpg"
+			active={0}
+		>
+			<Devenirconcessionnaire />
+		</Page>
+	);
+};
+
+export default Home;
