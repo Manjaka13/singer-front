d8829b4f0c249d74ce67307641e7d8f37b314558
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-12-18 10:41:45 +0100

    fix href url

--- components/Cardproduct.tsx
+++ components/Cardproduct.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {IProduct} from "helpers/interface";
+import { IProduct } from "helpers/interface";
 
 const Cardproduct: React.FC<IProduct> = ({
 	id,
@@ -8,13 +8,17 @@ const Cardproduct: React.FC<IProduct> = ({
 	price,
 	description,
 	promotion,
-	outstock
+	outstock,
 }): JSX.Element => (
 	<article className="card-product">
 		<figure className="card-product__representation">
 			<img className="card-product__image" src="/machine.jpg" alt="Machine" />
-			<p className={`card-product__badge card-product__badge${outstock ? '--red' : promotion ? '--green' : '--hidden'}`}>
-				{outstock ? 'En rupture de stock' : promotion ? 'En promotion' : ''}
+			<p
+				className={`card-product__badge card-product__badge${
+					outstock ? "--red" : promotion ? "--green" : "--hidden"
+				}`}
+			>
+				{outstock ? "En rupture de stock" : promotion ? "En promotion" : ""}
 			</p>
 		</figure>
 		<h3 className="card-product__title">{title}</h3>
@@ -22,8 +26,10 @@ const Cardproduct: React.FC<IProduct> = ({
 		<div className="card-product__separator"></div>
 		<p className="card-product__description">{description}</p>
 		<div className="card-product__discover-box">
-			<Link href={`details/${id}`} passHref>
-				<a className="card-product__discover" title="Découvrir ce produit">Découvrir</a>
+			<Link href={`/details/${id}`} passHref>
+				<a className="card-product__discover" title="Découvrir ce produit">
+					Découvrir
+				</a>
 			</Link>
 		</div>
 	</article>
