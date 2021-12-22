7092223e3e199a668f31d3fe7326432b2790570b
Rakotoarisoa Tahiriniaina Andrian <LenOkimaru@gmail.com>
2021-12-22 13:24:16 +0100

    loading component scss added

---
+++ components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = (): JSX.Element => (
+	<div className="loading">
+		<div></div>
+		<div></div>
+		<div></div>
+		<div></div>
+	</div>
+);
+
+export default Loading;
